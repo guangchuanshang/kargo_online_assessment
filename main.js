be7502515aa629da62ff9f91e6afbeac7f38340b
Guangchuan Shang <guangchuan.shang@berkeley.edu>
2021-04-28 23:51:47 -0700

    Added launcher code:call to main() and initialized global Map object to help translate the input integer into its appropriate word.

--- main.js
+++ main.js
@@ -15,3 +15,19 @@ Output: (String) string name of inputString.
 function helper(inputString) {
 
 }
+
+/* Initializes global map object to translate a string integer to its respective word. */
+let intMap = new Map();
+intMap.set("0", "Zero");
+intMap.set("1", "One");
+intMap.set("2", "Two");
+intMap.set("3", "Three");
+intMap.set("4", "Four");
+intMap.set("5", "Five");
+intMap.set("6", "Six");
+intMap.set("7", "Seven");
+intMap.set("8", "Eight");
+intMap.set("9", "Nine");
+
+/* Uses stdIn and launches main funtion to execute program with appropriate array slice. */
+main(process.argv.slice(2));
