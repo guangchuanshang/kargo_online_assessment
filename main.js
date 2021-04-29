18e25ce5ce0af2490eeca850b063e0ac214d725e
Guangchuan Shang <guangchuan.shang@berkeley.edu>
2021-04-28 23:45:49 -0700

    Created main.js and made skeleton for a main function and helper function.

---
+++ main.js
@@ -0,0 +1,17 @@
+/*
+Takes in a list of strings each corresponding to valid integers and prints those integers->stringName to stdOut.
+Input: (Array) inputList: list of strings corresponding to valid integers.
+Output: (stdOut) None: prints the string name of each integer digit.
+*/
+function main(inputList) {
+
+}
+
+/*
+Takes in a string of digits and returns the string name of that string of digits.
+Input: (String) inputString: string of digits.
+Output: (String) string name of inputString.
+*/
+function helper(inputString) {
+
+}
